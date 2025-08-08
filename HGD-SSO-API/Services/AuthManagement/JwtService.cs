6a59d9307b9e15501ec2942e28a45fcb2a15bfa1
amo-hada <amirhdv83@gmail.com>
2025-08-08 19:47:47 +0330

    deploying the JwtService.cs

---
+++ HGD-SSO-API/Services/AuthManagement/JwtService.cs
@@ -0,0 +1,40 @@
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using HGD_SSO_API.Models.UserManagement;
+using Microsoft.IdentityModel.Tokens;
+
+namespace HGD_SSO_API.Services.AuthManagement;
+
+public class JwtService
+{
+    private readonly IConfiguration _config;
+
+    public JwtService(IConfiguration config)
+    {
+        _config = config;
+    }
+
+    public string GenerateAccessToken(User user)
+    {
+        var claims = new[]
+        {
+            new Claim(JwtRegisteredClaimNames.Sub, user.Id.ToString()),
+            new Claim(JwtRegisteredClaimNames.PreferredUsername, user.UserName),
+            //new Claim("fullName", user.FullName)
+        };
+
+        var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Jwt:Key"]));
+        var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+        var token = new JwtSecurityToken(
+            issuer: _config["Jwt:Issuer"],
+            audience: _config["Jwt:Audience"],
+            claims: claims,
+            expires: DateTime.UtcNow.AddMinutes(15),
+            signingCredentials: creds
+        );
+
+        return new JwtSecurityTokenHandler().WriteToken(token);
+    }
+}
