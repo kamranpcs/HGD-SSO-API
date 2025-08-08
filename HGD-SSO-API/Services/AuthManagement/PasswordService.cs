133d6f0d46cb9f3df764f205ae37e9a282bda1f9
amo-hada <amirhdv83@gmail.com>
2025-08-08 19:47:36 +0330

    creating the PasswordService.cs

---
+++ HGD-SSO-API/Services/AuthManagement/PasswordService.cs
@@ -0,0 +1,19 @@
+using System.Security.Cryptography;
+using System.Text;
+
+namespace HGD_SSO_API.Services.AuthManagement;
+
+public class PasswordService
+{
+    public string HashPassword(string password)
+    {
+        using var sha256 = SHA256.Create();
+        var bytes = sha256.ComputeHash(Encoding.UTF8.GetBytes(password));
+        return Convert.ToBase64String(bytes);
+    }
+
+    public bool VerifyPassword(string password, string hash)
+    {
+        return HashPassword(password) == hash;
+    }
+}
