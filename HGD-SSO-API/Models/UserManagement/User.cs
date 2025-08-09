da8f405a2e96be87de2c91edfe748991ae3277bc
amo-hada <amirhdv83@gmail.com>
2025-08-09 19:50:54 +0330

    changing the UserName to Username in user entity

--- HGD-SSO-API/Models/UserManagement/User.cs
+++ HGD-SSO-API/Models/UserManagement/User.cs
@@ -4,7 +4,7 @@ namespace HGD_SSO_API.Models.UserManagement;
 
 public class User : BaseEntity
 {
-    public string UserName { get; set; }
+    public string Username { get; set; }
     public string PhoneNumber { get; set; }
     public string PasswordHash { get; set; }
 }
